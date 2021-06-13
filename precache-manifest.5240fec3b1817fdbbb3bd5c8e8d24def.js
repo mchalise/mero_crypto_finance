a154de4ef00b0853a93f665eab91c3a1dbd22acd
Manish Chalise <mchalise@gmail.com>
2021-06-13 17:12:33 +0545

    Updates

---
+++ precache-manifest.5240fec3b1817fdbbb3bd5c8e8d24def.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16f6ea7b32750b2909f3b6f5db289cf",
+    "url": "/mero_crypto_finance/index.html"
+  },
+  {
+    "revision": "cf7c3cb04a952a79e560",
+    "url": "/mero_crypto_finance/static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "8d794552714c51414ee2",
+    "url": "/mero_crypto_finance/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "cf7c3cb04a952a79e560",
+    "url": "/mero_crypto_finance/static/js/2.cef3b16e.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/mero_crypto_finance/static/js/2.cef3b16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d794552714c51414ee2",
+    "url": "/mero_crypto_finance/static/js/main.154438c1.chunk.js"
+  },
+  {
+    "revision": "dc1c99e94a1829fa038e",
+    "url": "/mero_crypto_finance/static/js/runtime-main.60d778ba.js"
+  },
+  {
+    "revision": "735673948d6450a11994a4b656c64457",
+    "url": "/mero_crypto_finance/static/media/dbank2.73567394.png"
+  }
+]);
